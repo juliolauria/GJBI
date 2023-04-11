e4efb08408251cad10934888bdbff39a8ebd18ec
Julio Vince Lauria <48496104+juliolauria@users.noreply.github.com>
2023-04-11 12:05:07 +0200

    Add files via upload

---
+++ Scrapers/#Jobs/GLPG_ANA_EXT_BGG_Job.bat
@@ -0,0 +1,13 @@
+D:
+
+CD Scrapers\bgg_scraper\
+
+call "D:\Program Files\Anaconda\Scripts\activate.bat"
+
+scrapy crawl boardgames
+
+python Scripts\GLPG_ANA_EXT_BGG_SalvarArquivo.py
+
+python Scripts\GLPG_ANA_EXT_BGG_Profile.py
+
+exit
---
+++ Scrapers/#Jobs/GLPG_ANA_EXT_Ludopedia_Jobs.bat
@@ -0,0 +1,17 @@
+D:
+
+CD Scrapers\ludopedia_scraper
+
+call "D:\Program Files\Anaconda\Scripts\activate.bat"
+
+scrapy crawl anuncios
+
+scrapy crawl ranking
+
+scrapy crawl rankingsespecificos
+
+scrapy crawl jogoslinks
+
+scrapy crawl jogos
+
+python Scripts\GLPG_ANA_EXT_Ludopedia_GravarArquivosGoogleDrive.py
